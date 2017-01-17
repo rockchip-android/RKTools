14a81ce3273a8f1bc3b4bb6fc5f17a4a4f5175c9
cw <cw@rock-chips.com>
2017-01-17 16:13:49 +0800

    RKTools: Remove unused files and adapt for build.sh
    Change-Id: I7190165843358eaa621d11fc579c72e66e21a7ff
    Signed-off-by: cw <cw@rock-chips.com>

--- linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
+++ linux/Linux_Pack_Firmware/rockdev/mkupdate.sh
@@ -6,17 +6,17 @@ read -n1 -s key
 exit 1
 }
 echo "start to make update.img..."
-if [ ! -f "parameter" ]; then
+if [ ! -f "parameter" -a ! -f "parameter.txt" ]; then
 	echo "Error:No found parameter!"
-	pause
+	exit 1
 fi
 if [ ! -f "package-file" ]; then
 	echo "Error:No found package-file!"
-	pause
+	exit 1
 fi
 ./afptool -pack ./ Image/update.img || pause
 ./rkImageMaker -RK322H MiniLoaderAll.bin Image/update.img update.img -os_type:androidos || pause
 echo "Making update.img OK."
-echo "Press any key to quit:"
-read -n1 -s key
-exit 0
+#echo "Press any key to quit:"
+#read -n1 -s key
+exit $?
